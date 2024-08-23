088a7c282477a9f4f07ba236ab9941fd18a764f2
ElectraBytes04 <electra-dev.404@protonmail.com>
2024-08-23 17:10:03 -0400

    Update info.py

--- include/info.py
+++ include/info.py
@@ -1,9 +1,6 @@
 appload_error = f"""
 --- /!\ Error /!\ ---
 Could not find the app you tried to load.
-Did you spell the name of the app correctly?
-Is there a Python file in the app's directory?
-Please check the app's directory.
 """
 
 command_help = """
